53e5bed27478c8989cf9d7612e217e1ce54237c4
Dade Sheng <hackerdade@gmail.com>
2022-10-02 11:03:02 +0800

    update main page to have selects with different properties

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,32 @@ const options = [
 function App() {
   return (
     <div className="App">
+      <h4>options</h4>
+      <Select options={options} placeholder="" value="" />
+
+      <h4>placeholder</h4>
+      <Select options={options} placeholder="Select..." value="" />
+
+      <h4>value</h4>
+      <Select options={options} placeholder="" value="orange" />
+
+      <h4>clearable</h4>
+      <Select options={options} placeholder="" value="forest" clearable />
+
+      <h4>multiple</h4>
+      <Select options={options} placeholder="" value="" clearable multiple />
+
+      <h4>styles</h4>
       <Select
-        placeholder="Select..."
+        styles={{
+          actionIcon: { color: 'orange' },
+          value: { color: 'green' },
+        }}
         options={options}
-        value="ocean"
         clearable
+        multiple
+        value=""
+        placeholder=""
       />
     </div>
   )
